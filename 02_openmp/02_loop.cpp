4c2779957ca6800af3711c8b4e03627e26555e64
nakano <nakano.m.aq@m.titech.ac.jp>
2023-04-24 00:38:04 +0900

    02_openmp

--- 02_openmp/02_loop.cpp
+++ 02_openmp/02_loop.cpp
@@ -2,7 +2,7 @@
 #include <omp.h>
 
 int main() {
-#pragma omp parallel for
+#pragma omp parallel for schedule(dynamic)
   for(int i=0; i<8; i++) {
     printf("%d: %d\n",omp_get_thread_num(),i);
   }
